c168af31b5d653c2407c3ef2977c688d424b3d3e
abcarlson12345 <abcarlson12345@gmail.com>
2022-12-18 10:50:30 -0800

    Update streamlit_app.py
    fixed emoji definition

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Favorites')
 
-streamlit.text('	u"\U0001F963" Omega 3 & Blueberry Oatmeal')
+streamlit.text(\U0001F963 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
