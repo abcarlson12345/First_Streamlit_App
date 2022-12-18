f5c8c862baaeb63b6063dd49a592ab32f6592409
abcarlson12345 <abcarlson12345@gmail.com>
2022-12-18 10:44:34 -0800

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.body('Kale, Spinach & Rocket Smoothie')
+streamlit.body('Hard-Boiled Free-Range Egg')
