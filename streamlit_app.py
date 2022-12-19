3092c65b304ca3fb00e92ca409c1c1760464986e
abcarlson12345 <abcarlson12345@gmail.com>
2022-12-19 11:35:27 -0800

    Update streamlit_app.py
    reorder outputs on ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,9 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
@@ -31,5 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this creates a table for viewing of the data
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
